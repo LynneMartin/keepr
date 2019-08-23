36b2ccb78b7bbe9bc9eca804e0c02e23fdacf76a
LynneMartin <lynnemartin@gmail.com>
2019-08-23 15:14:19 -0600

    added comments on what NOT to touch

--- wwwsrc/src/views/Home.vue
+++ wwwsrc/src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <h1>Welcome Home {{user.username}}</h1>
+    <!--NOTE If logged in, show logout button. If not logged in, show login button-->
     <button v-if="user.id" @click="logout">logout</button>
     <router-link v-else :to="{name: 'login'}">Login</router-link>
   </div>
