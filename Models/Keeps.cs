c6754e78b8a4809715462c293efe11fd0446ef00
LynneMartin <lynnemartin@gmail.com>
2019-08-25 19:33:56 -0600

    Models mostly set up, need review

---
+++ Models/Keeps.cs
@@ -0,0 +1,16 @@
+namespace keepr.Models
+{
+  public class Keeps
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Img { get; set; } //NOTE NOT REQUIRED? Adding anyway
+    public string Description { get; set; }
+
+    //NOTE REQUIRED: "Anytime a keep is viewed or kept in a vault, the count should go up"
+    public int KeepsCount { get; set; } //REVIEW naming convention ok?
+    //REVIEW Does there need to be a separate counter for # of keeps in a vault?
+
+
+  }
+}
---
+++ Models/Vault.cs
@@ -0,0 +1,10 @@
+namespace keepr.Models
+{
+  public class Vault
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    // public string Img { get; set; } //REVIEW is this used in vaults?
+    public string Description { get; set; }
+  }
+}
