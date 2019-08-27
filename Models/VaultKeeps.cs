dfe8bb694778a6341d30b4419cdea1665f9c7191
LynneMartin <lynnemartin@gmail.com>
2019-08-27 11:35:24 -0600

    added Model vaultkeeps.cs, in progress

---
+++ Models/VaultKeeps.cs
@@ -0,0 +1,14 @@
+namespace keepr.Models
+{
+  public class VaultKeeps
+  {
+    //id get set?
+    //user id get set
+    //vault id get set
+    //keep id get set
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public int VaultId { get; set; }
+    public int KeepId { get; set; }
+  }
+}
